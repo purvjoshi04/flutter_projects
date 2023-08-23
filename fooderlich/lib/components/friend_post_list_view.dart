8c45cffad3c9c8d298a5162a2b75cadac3fd3190
Purv Joshi <purvadjoshi@gmail.com>
2023-08-23 15:19:55 +0530

    Intial Commit

--- fooderlich/lib/components/friend_post_list_view.dart
+++ fooderlich/lib/components/friend_post_list_view.dart
@@ -1,52 +1,46 @@
 import 'package:flutter/material.dart';
+
+import '../components/components.dart';
 import '../models/models.dart';
-import 'components.dart';
 
 class FriendPostListView extends StatelessWidget {
-// 1
   final List<Post> friendPosts;
+
   const FriendPostListView({
     super.key,
     required this.friendPosts,
   });
+
   @override
   Widget build(BuildContext context) {
-// 2
     return Padding(
       padding: const EdgeInsets.only(
         left: 16,
         right: 16,
         top: 0,
       ),
-// 3
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-// 4
-          Text('Social Chefs 🧑‍🍳 ',
-              style: Theme.of(context).textTheme.displayLarge),
-// 5
-          const SizedBox(height: 16),
-// 1
-          ListView.separated(
-// 2
-            primary: false,
-// 3
-            physics: const NeverScrollableScrollPhysics(),
-// 4
-            shrinkWrap: true,
-            scrollDirection: Axis.vertical,
-            itemCount: friendPosts.length,
-            itemBuilder: (context, index) {
-// 5
-              final post = friendPosts[index];
-              return FriendPostTile(post: post);
-            },
-            separatorBuilder: (context, index) {
-// 6
-              return const SizedBox(height: 16);
-            },
+          Text(
+            'Social Chefs 👩‍🍳',
+            style: Theme.of(context).textTheme.displayLarge,
           ),
+          const SizedBox(height: 16),
+          ListView.separated(
+              primary: false,
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              scrollDirection: Axis.vertical,
+              itemCount: friendPosts.length,
+              itemBuilder: (context, index) {
+                final post = friendPosts[index];
+                return FriendPostTile(post: post);
+              },
+              separatorBuilder: (context, index) {
+                return const SizedBox(height: 16);
+              }),
+          const SizedBox(height: 16),
         ],
       ),
     );
