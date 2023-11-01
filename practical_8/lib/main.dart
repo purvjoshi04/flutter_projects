4a7a7dc98d112d9c786e2ece0027741c1a6a4235
Purv Joshi <purvadjoshi@gmail.com>
2023-11-01 12:29:30 +0530

    added practical 9

---
+++ practical_8/lib/main.dart
@@ -0,0 +1,87 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+   const MyHomePage({super.key});
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _textEditingController = TextEditingController();
+  String _username = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUsernameFromPrefs();
+  }
+
+  Future<void> _loadUsernameFromPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _username = prefs.getString('username') ?? '';
+    });
+  }
+
+  Future<void> _saveUsernameToPrefs(String username) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('username', username);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Practical 8'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Hello, $_username'), // Display greetings message
+            const SizedBox(height: 20), // Add space between the Text and TextField
+            TextField(
+              controller: _textEditingController,
+              decoration: const InputDecoration(labelText: 'Enter your name'),
+            ),
+            const SizedBox(height: 20), // Add space between the TextField and ElevatedButton
+            ElevatedButton(
+              onPressed: () {
+                final newUsername = _textEditingController.text;
+                _saveUsernameToPrefs(newUsername);
+                setState(() {
+                  _username = newUsername;
+                });
+              },
+              child: const Text('Save'),
+            ),
+          ],
+        ),
+      )
+
+    );
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+}
---
+++ practical_9/lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ProductListPage(),
+    );
+  }
+}
+
+class ProductListPage extends StatefulWidget {
+  const ProductListPage({super.key});
+
+  @override
+  _ProductListPageState createState() => _ProductListPageState();
+}
+
+class _ProductListPageState extends State<ProductListPage> {
+  List<dynamic> products = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchProductData();
+  }
+
+  Future<void> _fetchProductData() async {
+    final response = await http.get(Uri.parse('https://fakestoreapi.com/products'));
+
+    if (response.statusCode == 200) {
+      setState(() {
+        products = json.decode(response.body);
+      });
+    } else {
+      throw Exception('Failed to load product data');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Practical 9'),
+      ),
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          final product = products[index];
+          return Card(
+            child: ListTile(
+              leading: Image.network(product['image'], width: 80, height: 80), // Display the product image
+              title: Text(product['title']),
+              subtitle: Text('\$${product['price']}'),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
